d8abff8fece4e425be29fff6add200a6c93e4a8b
Adamito7 <adam.ondrus@gmail.com>
2023-01-10 22:57:11 +0100

    Database

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -1,14 +1,8 @@
 from django import forms
 
-SHIFTS = (
-    ('1', 'Morning'),
-    ('2', 'Afternoon'),
-    ('3', 'Evening'),
-)
+from .models import Logger
 
-class InputForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
-    shift = forms.ChoiceField(choices=SHIFTS)
-
+class LogForm(forms.ModelForm):
+    class Meta:
+        model = Logger
+        fields = '__all__'
