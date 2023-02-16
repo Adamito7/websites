f39547bdd153dd2b3619c22eed471238eabd725f
Adamito7 <adam.ondrus@gmail.com>
2023-02-16 11:33:40 +0100

    Updates

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('home/', views.form_view)
+    path('home/', views.form_view),
+    path('timer/', views.timer_view),
 ]
 
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from myapp.forms import LogForm
-form myapp.forms import TimerForm
+from myapp.forms import TimerForm
 
 # Create your views here.
 
@@ -18,8 +18,11 @@ def form_view(request):
 
 def timer_view(request):
     form = TimerForm()
-
-
-    context = {"form": form"}
+    if request.method == 'POST':
+        form = TimerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Form submission successful')
+    context = {"form": form}
     return render(request, 'timer.html',context)
 
